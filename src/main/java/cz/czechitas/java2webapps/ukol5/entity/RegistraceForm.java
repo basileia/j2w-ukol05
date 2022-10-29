9370467ff6c4e69d257e14784065af65a2cc1adc
basileia <jane.novak@centrum.cz>
2022-10-29 15:47:02 +0200

    class RegistraceForm with validations, enum Pohlavi

--- src/main/java/cz/czechitas/java2webapps/ukol5/entity/RegistraceForm.java
+++ src/main/java/cz/czechitas/java2webapps/ukol5/entity/RegistraceForm.java
@@ -1,10 +1,79 @@
 package cz.czechitas.java2webapps.ukol5.entity;
 
-import javax.validation.constraints.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 
 public class RegistraceForm {
 
     @NotBlank
     private String jmeno;
+    @NotBlank
+    private String prijmeni;
+    @NotBlank
+    private LocalDate datumNarozeni;
+    @NotNull
+    private Pohlavi pohlavi;
+    @NotNull
+    private String turnus;
+    @Email
+    private String email;
+    private String telefon;
 
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public String getPrijmeni() {
+        return prijmeni;
+    }
+
+    public void setPrijmeni(String prijmeni) {
+        this.prijmeni = prijmeni;
+    }
+
+    public LocalDate getDatumNarozeni() {
+        return datumNarozeni;
+    }
+
+    public void setDatumNarozeni(LocalDate datumNarozeni) {
+        this.datumNarozeni = datumNarozeni;
+    }
+
+    public Pohlavi getPohlavi() {
+        return pohlavi;
+    }
+
+    public void setPohlavi(Pohlavi pohlavi) {
+        this.pohlavi = pohlavi;
+    }
+
+    public String getTurnus() {
+        return turnus;
+    }
+
+    public void setTurnus(String turnus) {
+        this.turnus = turnus;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
 }
